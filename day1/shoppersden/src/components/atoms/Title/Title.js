09467e284704eac7dcf3c2c7f67d3a78c7e137ab
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-12-17 17:49:17 +0530

    updated

---
+++ day1/shoppersden/src/components/atoms/Title/Title.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Title.css';
+
+const Title = () => (
+  <div className="Title">
+    Title Component
+  </div>
+);
+
+Title.propTypes = {};
+
+Title.defaultProps = {};
+
+export default Title;
---
+++ day1/shoppersden/src/main.jsx
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import AppComponent from './App.jsx'
+
+createRoot(document.getElementById('ecommerce-root')).render(
+  <StrictMode>
+   <AppComponent/>
+  </StrictMode>,
+)

fc865f0d8219f55c7669b0659e6aba01caa0d19d
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-12-19 17:40:51 +0530

    updated

--- day1/shoppersden/src/components/molecules/Login/Login.jsx
+++ day1/shoppersden/src/components/molecules/Login/Login.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
-import { Box } from '@mui/material';
-
+import { Box, Button, TextField} from '@mui/material';
+import { Form } from 'formik';
 import './Login.css';
 
 function Login() {
+
+  const[userName,setUserName]=React.useState("");
+  const[password,setPassword]=React.useState("");
+  
+
   return (
     <Box sx={{ width: '100%', height: '100vh', bgcolor: "lightblue", display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-
+  
+    <Form>
+       <TextField fullWidth variant='outlined' label="UserName" name="userName" />
+        
+       
+       <TextField fullWidth variant='outlined' label="Password" name="password" type="password" />
+       
+       <Button variant="contained" color="primary" type="submit">
+        Submit
+        </Button>
+    </Form>
 
 
     </Box>

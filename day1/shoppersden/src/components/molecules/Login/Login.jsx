4f722b5d9334a01d5152180d9d8b4729162f453e
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-12-19 17:33:21 +0530

    updated

---
+++ day1/shoppersden/src/components/molecules/Login/Login.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Box } from '@mui/material';
+
+import './Login.css';
+
+function Login() {
+  return (
+    <Box sx={{ width: '100%', height: '100vh', bgcolor: "lightblue", display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+
+
+
+    </Box>
+  )
+}
+
+export default Login;

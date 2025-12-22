c55ac6089f6aaa9da2ffb849f008aaac18fbaa9f
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-12-22 16:34:38 +0530

    updated

--- day1/shoppersden/src/components/molecules/Login/Login.jsx
+++ day1/shoppersden/src/components/molecules/Login/Login.jsx
@@ -1,36 +1,54 @@
 import React from 'react';
 import { Box, Button, TextField} from '@mui/material';
 import InputAdornment from '@mui/material/InputAdornment';
-import AccountCircle from '@mui/icons-material/People';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import LockIcon from '@mui/icons-material/Lock';
-import IconButton from '@mui/material/IconButton';
-import Visibility from '@mui/icons-material/Visibility';
-import VisibilityOff from '@mui/icons-material/VisibilityOff';
-import showPassword from '@mui/icons-material/Visibility';
-import handleClickShowPassword from '@mui/icons-material/VisibilityOff';
-
+import * as Yup from 'yup';
 import { useFormik } from 'formik'; 
 import { Form } from 'formik';
 import './Login.css';
 
 function Login() {
 
-  const[userName,setUserName]=React.useState("");
-  const[password,setPassword]=React.useState("");
+  /* const[userName,setUserName]=React.useState("");
+  const[password,setPassword]=React.useState(""); */
+
+  const validationSchema=Yup.object({
+    userName:Yup.string().required("UserName is required"),
+    password:Yup.string().required("Password is required")
+  })
+
+  const formik=useFormik({
+    initialValues:{
+      userName:"",
+      password:""
+    },
+    validationSchema:validationSchema,
+    onSubmit:(values)=>{
+      console.log("Form data",values);
+    }
    
+  })
+   
+  
 
   return (
     <Box sx={{ width: '40%', height: '50vh', marginRight: '3%', display: 'flex', float: 'right', alignItems: 'center' }}>
   
-    <form>
+    <form onSubmit={formik.handleSubmit} style={{width:"100%"}}>
       <fieldset className='border-purple-700 border-2 rounded-md p-4'>
         <legend className='text-xl text-center   font-semibold text-purple-700'>Login</legend>
        <TextField fullWidth variant='outlined' label="UserName" 
-       name="userName" sx={{padding:"10px"}}  slotProps={{
+       name="userName" sx={{padding:"10px"}} values={formik.values.userName}
+        onChange={formik.handleChange} 
+         onError={formik.touched.userName && formik.errors.userName && Boolean(formik.errors.userName)}
+         helperText={formik.touched.userName &&formik.errors.userName}
+        
+        slotProps={{
     input: {
       startAdornment: (
         <InputAdornment position="start">
-         <AccountCircle />
+         <AccountCircleIcon />
         </InputAdornment>
       )
     }
@@ -38,7 +56,10 @@ function Login() {
         
        
        <TextField fullWidth variant='outlined' label="Password" name="password" type="password"
-         sx={{padding:"10px"}}  slotProps={{
+         sx={{padding:"10px"}} values={formik.values.password} onChange={formik.handleChange} 
+         onError={formik.touched.password && formik.errors.password && Boolean(formik.errors.password)}
+         helperText={formik.touched.password &&formik.errors.password}
+         slotProps={{
     input: {
       startAdornment: (
         <InputAdornment position="start">

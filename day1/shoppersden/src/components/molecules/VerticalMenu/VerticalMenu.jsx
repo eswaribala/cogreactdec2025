154982e8bc4e32192ddc7d7826f76249b512dcbf
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-12-24 17:22:55 +0530

    updated

--- day1/shoppersden/src/components/molecules/VerticalMenu/VerticalMenu.jsx
+++ day1/shoppersden/src/components/molecules/VerticalMenu/VerticalMenu.jsx
@@ -4,6 +4,7 @@ import './VerticalMenu.css';
 import Box from '@mui/material/Box';
 import Tab from '@mui/material/Tab';
 import { TabContext, TabList, TabPanel } from '@mui/lab';
+import Grid from '@mui/material/Grid';
 
 function VerticalMenu() {
   const[value, setValue] = useState('1');
@@ -13,15 +14,17 @@ function VerticalMenu() {
   }
 
   return(
-    <Box sx={{ width: '100%', typography: 'body1' }}>
+    <Grid container spacing={2}>
       <TabContext value={value}>
-        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Grid>
           <TabList orientation="vertical" variant="scrollable" onChange={handleChange} aria-label="Vertical tabs example" sx={{ borderRight: 1, borderColor: 'divider' }}>
             <Tab label="Books" value="1"></Tab>
             <Tab label="Gifts" value="2"></Tab>
             <Tab label="Clothing" value="3"></Tab>
             <Tab label="Sports" value="4"></Tab>
           </TabList>
+        </Grid>
+        <Grid>
           <TabPanel value="1">
             Books Content
             
@@ -36,9 +39,9 @@ function VerticalMenu() {
           <TabPanel value="4">
             Sports Content  
           </TabPanel>
-        </Box>
+        </Grid>
       </TabContext>
-    </Box>
+    </Grid>
   )
 
 }

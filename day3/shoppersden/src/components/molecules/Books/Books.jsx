5249b11b26cc8feb48ab87ee4064c6385a1be0a2
Parameswari Ettiappan <parameswaribala@gmail.com>
2025-12-29 17:24:41 +0530

    updated

--- day3/shoppersden/src/components/molecules/Books/Books.jsx
+++ day3/shoppersden/src/components/molecules/Books/Books.jsx
@@ -8,11 +8,20 @@ function Books() {
   const [limit, setLimit] = React.useState(5);
   
   React.useEffect(() => {
-    fetch(`${import.meta.env.VITE_BOOKS_BaseURL}?_page=${page}&_limit=${limit}`)
+    fetch(`${import.meta.env.VITE_BOOKS_BaseURL}?page=${page}&limit=${limit}`)
       .then((res) => res.json())
-      .then((data) => setItems(data))
+      .then((data) => {
+        setItems(data)
+        console.log(data);
+      })
       .catch((err) => console.error('Error fetching books:', err));
   }, [page, limit]);
+
+  return(
+    <div className="books-container">
+      <h2>Books List</h2>
+     </div>
+  )
 }
 
 

b6d629900bf658b196e3d421b1f12ad97cc59b4b
Parameswari Ettiappan <parameswaribala@gmail.com>
2026-01-02 16:25:38 +0530

    updated

--- day3/shoppersden/src/components/molecules/Clothing/Clothing.jsx
+++ day3/shoppersden/src/components/molecules/Clothing/Clothing.jsx
@@ -1,13 +1,38 @@
 import React from 'react';
-
+import {useState,useEffect} from 'react';
+import ClothingView from '../../views/ClothingView/ClothingView';
+import withSearchFilter from '../../../HOCs/withSearchFilter/withSearchFilter';
 import './Clothing.css';
 
-const Clothing = () => (
-  <div className="Clothing">
-    Clothing Component
-  </div>
-);
+function Clothing() {
+  const[clothes,setClothes]=useState([]);
+  const[loading,setLoading]=useState(true);
+  const[error,setError]=useState(false);
+  const ClothingViewUI=withSearchFilter(ClothingView,"Clothing");
+  useEffect(()=>{
+    fetch(import.meta.env.VITE_CLOTHES_API)
+    .then((res)=>res.json())
+    .then((data)=>{
+      setClothes(data.data);
+      console.log(data.data);
+      setLoading(false);
+    })
+    .catch((err) => {
+      setError(true);
+      setLoading(false);
+    });
+  },[]);
 
+  if(loading){
+    return <div>Loading...</div>;
+  }
+
+  if(error){
+    return <div>Error fetching clothing data.</div>;
+  }
+  return <ClothingViewUI items={clothes} />;
+
+}
 
 
 export default Clothing;

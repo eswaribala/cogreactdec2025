1c43fe671d03a7fc7f7df8a40a5469d014647a11
Parameswari Ettiappan <parameswaribala@gmail.com>
2026-01-02 16:12:46 +0530

    updated

---
+++ day3/shoppersden/src/components/molecules/ClothingView/ClothingView.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './ClothingView.css';
+
+const ClothingView = () => (
+  <div className="ClothingView">
+    ClothingView Component
+  </div>
+);
+
+ClothingView.propTypes = {};
+
+ClothingView.defaultProps = {};
+
+export default ClothingView;
---
+++ day3/shoppersden/src/components/molecules/withSearchFilter/withSearchFilter.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './withSearchFilter.css';
+
+const withSearchFilter = () => (
+  <div className="withSearchFilter">
+    withSearchFilter Component
+  </div>
+);
+
+withSearchFilter.propTypes = {};
+
+withSearchFilter.defaultProps = {};
+
+export default withSearchFilter;

f8513fb8c9dffb4cb811af8609fd9973c3644faa
Parameswari Ettiappan <parameswaribala@gmail.com>
2026-01-06 17:22:55 +0530

    updated

--- day3/shoppersden/src/redux/reducers/rootReducer.jsx
+++ day3/shoppersden/src/redux/reducers/rootReducer.jsx
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import productReducer   from "./productReducer.jsx";
+import registerReducer from "./registerReducer.jsx";
 const rootReducer = combineReducers({
-    productState: productReducer
+    productState: productReducer,
+    registerState: registerReducer
 });
 export default rootReducer;

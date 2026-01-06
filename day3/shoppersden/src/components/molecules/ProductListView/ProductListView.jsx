7c32b98ddc430e7ac23ce945eb0a9d4d46900ecb
Parameswari Ettiappan <parameswaribala@gmail.com>
2026-01-06 16:28:47 +0530

    updated

--- day3/shoppersden/src/components/molecules/ProductListView/ProductListView.jsx
+++ day3/shoppersden/src/components/molecules/ProductListView/ProductListView.jsx
@@ -2,12 +2,29 @@ import React from 'react';
 
 import './ProductListView.css';
 
-const ProductListView = () => (
-  <div className="ProductListView">
-    ProductListView Component
-  </div>
-);
-
+function ProductListView() {
+   const {loading,error,products}=useSelector((state)=>state.productState);
+   if(loading){
+    return <div>Loading products...</div>;
+   }
+   if(error){
+    return <div>Error loading products: {error}</div>;
+   }
+   return(
+    <Box>
+    <Typography variant="h4" gutterBottom sx={{textAlign:"center",mb:3}}>
+        Product List View
+    </Typography>
+    <List>
+        {products.slice(0, 5).map((product)=>(
+            <ListItem key={product.id}>
+                <ListItemText primary={product.name} secondary={`Category: ${product.category} - Price: $${product.price}`}/>
+            </ListItem>
+        ))}
+    </List>
+    </Box>
+   )
+}
 
 
 export default ProductListView;

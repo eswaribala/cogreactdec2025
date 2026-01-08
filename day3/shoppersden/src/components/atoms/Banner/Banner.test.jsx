47ebce682bd8b32855f1f4989d4d07e9acc1af7f
Parameswari Ettiappan <parameswaribala@gmail.com>
2026-01-08 17:01:01 +0530

    updated

--- day3/shoppersden/src/components/atoms/Banner/Banner.test.jsx
+++ day3/shoppersden/src/components/atoms/Banner/Banner.test.jsx
@@ -1,6 +1,7 @@
 import {render, screen} from '@testing-library/react';
+import {describe, it,expect} from 'vitest';
 import Banner from './Banner';
-import { it } from 'vitest';
+
 
 describe('Banner Component', () => {
 
